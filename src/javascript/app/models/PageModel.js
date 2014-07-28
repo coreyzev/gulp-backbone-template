9b9c6c5228ce8b41196443ed9a9eb592f2a8bdc4
Corey Zev <coreyzev@zevdesigns.com>
2014-07-28 16:54:58 -0400

    moving data fetch into the model, adding comments

--- src/javascript/app/models/PageModel.js
+++ src/javascript/app/models/PageModel.js
@@ -4,15 +4,42 @@ var App = require('../start.js'),
 
 module.exports = Backbone.Model.extend({
 
-    initialize:function () {
+    initialize: function() {
+        var model = this;
+        model.set({nextSlide: {}});
+        model.fetch({
+            success: function(data) {
+                model.updateNextSlide(data);
+            }
+        });
+        model.save();
+        App.Models.Instances[this.attributes.pageSlug] = this;
         this.reports = new Parents();
         this.reports.parent = this;
     },
+    updateNextSlide: function(data) {
+        var next = this.attributes.nextSlide,
+            nextObj = {},
+            model = this;
 
+        next.id = this.id + 1;
+
+        pageAdapter.findById(next.id)
+            .done(function(data) {
+                nextObj = data;
+                next.slug = nextObj.pageSlug;
+                next.title = nextObj.title;
+            })
+            .fail(function() {
+                model.unset("nextSlide", {
+                    silent: false
+                });
+            });
+    },
 
     sync: function(method, model, options) {
         if (method === "read") {
-            pageAdapter.findBySlug(this.attributes.pageSlug).done(function (data) {
+            pageAdapter.findBySlug(this.attributes.pageSlug).done(function(data) {
                 options.success(data);
             });
         }
--- src/javascript/app/routers/Router.js
+++ src/javascript/app/routers/Router.js
@@ -32,9 +32,7 @@ module.exports = Backbone.Router.extend({
 
         document.title = "CZ Updated Backbone App";
 
-        App.Collections.Instances.pages = new Pages();
         App.Models.Instances.home = new Page({ pageSlug: "home" });
-        App.model("home").fetch();
         var homePage = { model: App.model("home") };
 
         Backbone.Layout.configure({
@@ -43,7 +41,11 @@ module.exports = Backbone.Router.extend({
         });
         var mainLayout = App.Layouts.Instances.mainLayout = new Backbone.Layout({
             template: MainViewTmp,
-            el: '.container'
+            el: '.container',
+            addHF: function () {
+                $('.scroller-inner').prepend('<header id="header" class="bar bar-nav"></header>').append('<footer id="footer" class="bar bar-tab"></footer>');
+                $('#mp-pusher').prepend('<nav id="mp-menu" class="mp-menu"></nav>');
+            },
         });
 
         mainLayout.render().promise().done(function() {
@@ -59,14 +61,19 @@ module.exports = Backbone.Router.extend({
                 });
             });
         });
+
+        App.Collections.Instances.pages = new Pages(JSON.parse(window.localStorage.getItem("pages")));
     },
 
     linkClick: function (target, trigger) {
+        //Filter a link clicks
         if ($(target).data("bypass")) {
             if (target.href !== "") {
+                // If the target has "data-bypass=true" & the href isnt empty
                 window.location.href = target.href;
             }
         } else {
+            // else navigate using the backbone app
             this.navigate(target.hash, true);
         }
     },
@@ -80,79 +87,43 @@ module.exports = Backbone.Router.extend({
         var slug = slug1;
         for (var s = 1; s < arguments.length; s++) {
             if (arguments[s]) {
-                console.log(arguments[s]);
                 slug = slug + "/" + arguments[s];
-                console.log(slug);
             }
         }
-        pageAdapter.findBySlug(slug)
-            .done(function(data) {
-                var page;
-                if (!App.model(slug)) {
-                    page = App.Models.Instances[slug] = new Page({
-                        pageSlug: slug
-                    });
-                    console.warn("Created Instance for model:", slug);
-                } else {
-                    page = App.model(slug);
-                }
-                page.fetch({
-                    //fix this to use layoutManager
-                    success: function(data) {
-                        page.set({nextSlide:{}});
-                        function updateNextSlide() {
-                            var next = page.attributes.nextSlide,
-                                nextObj = {},
-                                model = page;
-
-                            next.id = page.id + 1;
-
-                            pageAdapter.findById(next.id)
-                                .done(function(data) {
-                                    nextObj = data;
-                                    next.slug = nextObj.pageSlug;
-                                    next.title = nextObj.title;
-                                })
-                                .fail(function() {
-                                    model.unset("nextSlide", {
-                                        silent: false
-                                    });
-                                });
-                        }
-                        updateNextSlide();
-
-                        var layout = App.layout('mainLayout');
-                        layout.removeView('header');
-                        layout.removeView('footer');
-                        layout.getView('#content').removeView('.sliderContent');
-                        layout.removeView('#mp-menu');
-                        $('.scroller-inner').prepend('<header id="header" class="bar bar-nav"></header>').append('<footer id="footer" class="bar bar-tab"></footer>');
-                        $('#mp-pusher').prepend('<nav id="mp-menu" class="mp-menu"></nav>');
-                        layout.getView('#content').setView('.sliderContent', new SliderPageView({ model: data })).render();
-                        layout.setView('header', new HeaderView({ model: data })).render();
-                        layout.setView('footer', new FooterView({ model: data })).render();
-                        layout.setView('#mp-menu', new SideNavView({ model: data })).render();
-                    }
-                });
-            })
-            .fail(function(data) {
-                App.router("router").pageDNE(slug);
-            });
-
+        if (App.model(slug)) {
+            //If the slug is found in the local database
+            var page = App.model(slug),
+                layout = App.layout('mainLayout');
+            //Remove the views from the DOM & stop listening to events
+            layout.removeView('header');
+            layout.removeView('footer');
+            layout.getView('#content').removeView('.sliderContent');
+            layout.removeView('#mp-menu');
+            //Add back in the missing elements
+            layout.addHF();
+            //Insert & render the views with the new data
+            layout.getView('#content').setView('.sliderContent', new SliderPageView({ model: page })).render();
+            layout.setView('header', new HeaderView({ model: page })).render();
+            layout.setView('footer', new FooterView({ model: page })).render();
+            layout.setView('#mp-menu', new SideNavView({ model: page })).render();
+        } else {
+            //If it's not found, navigate to the 404 page
+            App.router("router").pageDNE(slug);
+        }
     },
 
     pageDNE: function(wrongHash) {
         var layout = App.layout('mainLayout');
-        Backbone.Layout.cleanViews([
-            layout.getView('header'),
-            layout.getView('footer'),
-            layout.getView('#content').getView('.sliderContent'),
-            layout.getView('#mp-menu'),
-        ]);
-        layout.getView('#content').insertView('.sliderContent', new DNEView({ wrongHash: wrongHash })).render();
-        layout.insertView('header', new HeaderView({ DNE: true })).render();
-        layout.insertView('footer', new FooterView({ DNE: true })).render();
-        layout.insertView('#mp-menu', new SideNavView({ DNE: true })).render();
+        //Same as in the "page" function
+        layout.removeView('header');
+        layout.removeView('footer');
+        layout.getView('#content').removeView('.sliderContent');
+        layout.removeView('#mp-menu');
+        layout.addHF();
+        layout.getView('#content').setView('.sliderContent', new DNEView({ wrongHash: wrongHash })).render();
+        layout.setView('header', new HeaderView({ DNE: true })).render();
+        layout.setView('footer', new FooterView({ DNE: true })).render();
+        layout.setView('#mp-menu', new SideNavView({ DNE: true })).render();
     }
 
 });
