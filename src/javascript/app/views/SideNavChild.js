3065e9e62649dfb63eb53efd11d569a692f9f85c
Corey Zev <coreyzev@zevdesigns.com>
2014-07-29 03:49:13 -0400

    updating dynamic side nav

--- src/javascript/app/views/SideNavChild.js
+++ src/javascript/app/views/SideNavChild.js
@@ -5,20 +5,5 @@ var App = require('../start.js'),
 module.exports = Backbone.View.extend({
     manage: true,
     template: NavChildTmp,
-    tagName: 'li',
-    buildNav: function(){
-        /*
-        var layout = this;
-        App.collection("pages").each(function(model) {
-            if (model.attributes.parentId === 0 && model.id > 1) {
-                var y = model.attributes;
-                if (y.hasChildren) {
-                    layout.setView(new ParentWrapper(y.toJson()));
-
-                }
-                data["nav"].push({hash: y.pageSlug, title: y.title});
-            }
-        });
-        */
-    }
+    tagName: 'li'
 });
